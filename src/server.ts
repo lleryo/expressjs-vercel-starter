e625a4af0f4fd75f16626228d2f342b40d3494bc
Rocky Essel <rockyessel76@gmail>
2023-12-10 19:47:11 +0000

    changes: refactored package.json and vercel.json

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import dotenv from 'dotenv';
 import path from 'path';
 import helmet from 'helmet';
+
+
 import TTS from './routes/tts';
 import File from './routes/file';
 import Files from './routes/files';
@@ -12,16 +13,14 @@ const PORT = process.env.PORT || 8000;
 
 const server = express();
 
-dotenv.config({ path: '.env' });
-
 server.use(morgan('dev'));
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
-server.use(express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, '../public')));
 
-console.log('Dir: ', path.join(__dirname, 'public'));
+console.log('Dir: ', path.join(__dirname, '../public'));
 
 server.use('/api/v1/files', Files);
 server.use('/api/v1/file', File);
