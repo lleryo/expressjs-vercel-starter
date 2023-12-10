bb04c98b57d79f9442b5dd91951fac7329032805
Rocky Essel <rockyessel76@gmail>
2023-12-10 13:51:01 +0000

    base commit

--- app/src/routes/file.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { upload } from '../lib/config/multer';
-import { PostFile } from '../controllers/file';
-
-const router = express.Router();
-router.post('/', upload.single('file'), PostFile);
-
-export = router;
--- app/src/routes/files.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { upload } from '../lib/config/multer';
-import { PostFiles } from '../controllers/files';
-
-const router = express.Router();
-
-router.post('/', upload.array('files', 5), PostFiles);
-
-export = router;
